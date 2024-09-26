3c7db7c0ed817f61cc854b6c9ca7139be2f9aba3
ftl-model <ftl21918@163.com>
2024-09-26 20:01:11 +0800

    xgb_no_normalisation.py

--- xgb_no_normalisation.py
+++ xgb_no_normalisation.py
@@ -42,7 +42,7 @@ if st.button("Predict"):
     predicted_class = model.predict(features)[0]   
     predicted_proba = model.predict_proba(features)[0]
     # Display prediction results    
-    st.write(f"**Predicted Class:** {predicted_class}")   
+    st.write(f"**Predicted Class:** {predicted_class} (1: Disease, 0: No Disease)")   
     st.write(f"**Prediction Probabilities:** {predicted_proba}")
     # Generate advice based on prediction results  
     probability = predicted_proba[predicted_class] * 100
@@ -67,9 +67,13 @@ if st.button("Predict"):
     st.write(advice)
 
 # Calculate SHAP values and display force plot   
-    explainer = shap.TreeExplainer(model)    
+    st.subheader("SHAP Force Plot Explanation")
+    explainer_shap = shap.TreeExplainer(model)    
     shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
    
-    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)   
-    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
-    st.image("shap_force_plot.png")
+    if predicted_class == 1:        
+       shap.force_plot(explainer_shap.expected_value[1], shap_values[:,:,1], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)    
+    else:        
+        shap.force_plot(explainer_shap.expected_value[0], shap_values[:,:,0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)    
+        plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)    
+        st.image("shap_force_plot.png", caption='SHAP Force Plot Explanation')
